2273fab95b1920c882c9a3a33408968842d5573c
Ante Perisic <perisic.ante@gmail.com>
2022-05-10 09:38:03 +0200

    add state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 const App = () => {
 
-  const Person = (props) => {
-    return (
-      <>
-      <h1>Name: {props.name}</h1>
-      <h2>Last name: {props.lastname}</h2>
-      <h2>Age: {props.age}</h2>
-      </>
-    )
-  }
-  const name = "Ante";
-  const isNameShowing = false;
+  
   return (
     <div className="App">
-      <Person name = {'John'} lastname = {"Doe"} age = "30" ></Person>
-      <Person name ="Mary" lastname = "Doe"></Person>
+     
       
     </div>
   );
