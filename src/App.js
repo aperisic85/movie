8ffa694a8f131315fb538cd3d4f22c0f16522b8c
Ante Perisic <perisic.ante@gmail.com>
2022-05-10 13:56:00 +0200

    connect to mivie api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState} from 'react'
+import React from "react";
+import { useEffect } from "react";
+
+
+//a177f867
+
+const API_URL='http://www.omdbapi.com?apikey=a177f867';
 
 const App = () => {
+   
+    const searchMovies = async (title) =>{
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
 
-  const[counter, setCounter] = useState(0); 
-  
-  return (
-    <div className = "App">
-      <button onClick={() => setCounter((preCount) => preCount - 1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((preCount) => preCount + 1)}>+</button>
-      
-    </div>
-  );
+        console.log(data.Search);
+    }
+   
+    useEffect(()=>{
+        searchMovies('Spiderman');
+   }, []);
+
+   return (
+       <h1>Hi there</h1>
+   );
 }
 
 export default App;
